8c453aa92bb96b91c308891f2c2ef310252b2be3
fahdaguenouz <faguenouz@gmail.com>
2025-10-21 17:09:01 +0100

    starting the front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Login } from './auth/login/login.component';
+import { Home } from './home/home.component';
 
-export const routes: Routes = [];
+
+    export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Login }
+];
---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'home-page',
+  imports: [RouterOutlet],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class Home {
+ 
+}
