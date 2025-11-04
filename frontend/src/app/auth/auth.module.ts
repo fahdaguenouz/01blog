bb306b02278cf00ed6df0b56490ec2428e593921
fahdaguenouz <faguenouz@gmail.com>
2025-11-04 17:06:27 +0100

    changing to black theme

--- frontend/src/app/auth/auth.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { LoginComponent } from './login/login.component';
-import { SignUpComponent } from './signup/signup.component';
-
-@NgModule({
-  imports: [LoginComponent, SignUpComponent],
-  exports: [LoginComponent, SignUpComponent],
-})
-export class AuthModule {}
--- frontend/src/app/shared/navabr/navbar.component.ts
+++ frontend/src/app/shared/navabr/navbar.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
 import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, NgIf, MatToolbarModule, MatButtonModule],
+  imports: [RouterLink, RouterLinkActive, NgIf],
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
