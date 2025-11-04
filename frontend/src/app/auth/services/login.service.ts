960cb59e63d1170b83f83ef483f9e149e3dcdb7e
fahdaguenouz <faguenouz@gmail.com>
2025-11-04 17:44:13 +0100

    adding proxy

--- frontend/src/app/auth/services/login.service.ts
+++ frontend/src/app/auth/services/login.service.ts
@@ -8,7 +8,7 @@ import { AuthService } from '../../services/auth.service';
   providedIn: 'root'
 })
 export class LoginService {
-  apiUrl: string = "http://localhost:8080/api/auth"
+  apiUrl: string = "/api/auth"
 
   constructor(private httpClient: HttpClient, private authService: AuthService) { }
 
