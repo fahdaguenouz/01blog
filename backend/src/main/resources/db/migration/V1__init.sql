c4cc20bd170f975f787cde64446ca425e277573e
fahdaguenouz <faguenouz@gmail.com>
2025-11-30 15:14:12 +0100

    removing unused table

--- backend/src/main/resources/db/migration/V1__init.sql
+++ backend/src/main/resources/db/migration/V1__init.sql
@@ -25,11 +25,7 @@ CREATE TABLE users (
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
-CREATE TABLE user_roles (
-  user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-  role_id INT NOT NULL REFERENCES roles(id) ON DELETE CASCADE,
-  PRIMARY KEY (user_id, role_id)
-);
+
 
 -- MEDIA next (references users)
 CREATE TABLE media (
